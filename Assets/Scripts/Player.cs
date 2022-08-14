0fb50dd9f6f11100ab0f481deed1ed5b71b8471d
Oleg Kotov <onkotov@gmail.com>
2022-08-14 15:18:57 +0500

    Сделано управление игроком

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,49 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    
+    public float moveSpeed;
+    public float jumpForce;
+
+    public Rigidbody rig;
+
+    private void Update()
+    {
+        Move();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            Jump();
+    }
+
+    private void Move()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        //Vector3 dir = new Vector3(x, 0f, z) * moveSpeed;
+        Vector3 dir = transform.right * x + transform.forward * z;
+        dir *= moveSpeed;
+        dir.y = rig.velocity.y;
+
+        rig.velocity = dir;
+    }
+
+    void Jump()
+    {
+        if (CanJump())
+        {
+            rig.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+
+    bool CanJump()
+    {
+        Ray ray = new Ray(transform.position, Vector3.down);
+        RaycastHit hit;
+
+        if(Physics.Raycast(ray, out hit, 0.1f))
+        {
+            return hit.collider != null;
+        }
+        return false;
+    }
 }
