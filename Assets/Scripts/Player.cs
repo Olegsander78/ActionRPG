2b2a117aa8db92f77f3955607b5a6d83ab1d925a
Oleg Kotov <onkotov@gmail.com>
2022-08-17 00:44:09 +0500

    Сделано боевая механика

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    public int curHP;
+    public int maxHP;
+    
     public float moveSpeed;
     public float jumpForce;
+
+    public float attackRange;
+    public int damage;
+    private bool isAttacking;
 
     public Rigidbody rig;
 
@@ -15,6 +23,9 @@ public class Player : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
             Jump();
+
+        if(Input.GetMouseButtonDown(0) && !isAttacking)
+            Attack();
     }
 
     private void Move()
@@ -48,5 +59,37 @@ public class Player : MonoBehaviour
             return hit.collider != null;
         }
         return false;
+    }
+
+    public void TakeDamage(int damageToTake)
+    {
+        curHP -= damageToTake;
+        if (curHP <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    void Attack()
+    {
+        isAttacking = true;
+
+        Invoke("TryDamage", 0.7f);
+        Invoke("DisableIsAttacking", 1.5f);
+    }
+    void TryDamage()
+    {
+        Ray ray = new Ray(transform.position + transform.forward, transform.forward);
+        RaycastHit[] hits = Physics.SphereCastAll(ray, attackRange, 1 << 6);
+
+        foreach (var hit in hits)
+        {
+            hit.collider.GetComponent<Enemy>().TakeDamage(damage);
+        }
+        
+    }
+    void DisableIsAttacking()
+    {
+        isAttacking = false;
     }
 }
